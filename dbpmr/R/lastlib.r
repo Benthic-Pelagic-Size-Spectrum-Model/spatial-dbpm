ec83b2c95bcaed066269a1ba06b507f69c95a8a6
Julia Blanchard <julia.blanchard@utas.edu.au>
2026-06-25 11:34:06 +1000

    Refactor unloading process in lastlib.r
    Removed the .Last.lib function and its associated method and class removals, replacing it with a simpler .onUnload function.

--- dbpmr/R/lastlib.r
+++ dbpmr/R/lastlib.r
@@ -1,27 +1,3 @@
-.Last.lib <- function(libpath)
-{
-  
-  removeMethod("show","run.params")
-  removeMethod("show","grid.params")
-  removeMethod("show","pelagic.params")
-  removeMethod("show","benthic.params")
-  removeMethod("show","plankton.params")
-  removeMethod("show","detritus.params")  
-
-
-  removeClass("run.params")
-  removeClass("grid.params")
-  removeClass("pelagic.params")
-  removeClass("benthic.params")
-  removeClass("plankton.params")
-  removeClass("detritus.params")
-  removeClass("pelagic.results")
-  removeClass("benthic.results")
-  removeClass("plankton.results")
-  removeClass("detritus.results")
-  removeClass("timestep.data")
-  
-  library.dynam.unload("dbpmr",libpath)
-  cat("dbpmr 0.1-1 unloaded\n")
-  
+.onUnload <- function(libpath) {
+  library.dynam.unload("dbpmr", libpath)
 }
